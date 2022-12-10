1bade761b1971c0f8366b547896207cae455807f
NneomaOkoye <okoyenneoma1@gmail.com>
2022-12-09 17:14:42 -0800

    print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return - Always 0 (sucess)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while 
+		(c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return(0);
+}
