d4cbd763c9e41386ec4c0d89d867d9a16225792b
NneomaOkoye <okoyenneoma1@gmail.com>
2022-12-11 21:00:02 -0800

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,24 @@
+
 #include <stdio.h>
-#include <unistd.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 1(Success)
+ * main -prints to string
+ *Description: Prints "and that pice of art is useful.."without puts
+ * Return: 1
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\ - Dora Korpar, 2015-10-19\n", 59);
+	char *s = "and that piece of art is useful\" Dora Korpar, 2015-10-19\n";
+	long l = 59;
+	long fd = 1;
+	long syscall = 1;
+	long ret = 0;
+	_asm_ ("syscall"
+			: "=a" (ret)
+			: "a" (syscall),
+			"D" (fd),
+			"S" (s),
+			"d" (1);
 	return (1);
 }
