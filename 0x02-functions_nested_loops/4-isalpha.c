402a543f0c6782e8973dde190f18093b4ab96433
NneomaOkoye <okoyenneoma1@gmail.com>
2022-12-13 20:30:11 -0800

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isalpha - Enrty point
+ * @c: the integer value recieves
+ * Description: check if alphabet
+ * Return: 1 if true. 0 if false
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
