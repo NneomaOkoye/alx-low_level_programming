3fdc6a0de7e1680f67afb2ee32efbbfc37ed9df4
NneomaOkoye <okoyenneoma1@gmail.com>
2022-12-16 13:26:58 -0800

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,9 +18,9 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i == 100)
-			printf("%s", b);
+			printf("%s ", b);
 		else if ((i % 3 == 0) && (i % 5 == 0))
-			printf("%s", fb);
+			printf("%s ", fb);
 		else if (i % 3 == 0)
 			printf("%s ", f);
 		else if (i % 5 == 0)
