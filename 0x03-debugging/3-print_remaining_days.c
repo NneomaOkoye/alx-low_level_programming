59fed4a89ceaee0a152ecaaa0747951f3242e9d2
NneomaOkoye <okoyenneoma1@gmail.com>
2022-12-15 17:46:17 -0800

    last

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -12,14 +11,20 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+	/**
+	 * leap year when it can be be divided
+	 * by 100 and 400 or by 4 evenly
+	 */
+	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
-		if (month >= 2 && day >= 60)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
+
 		printf("Day of the year: %d\n", day);
 		Printf("Remaining days: %d\n", 366 - day);
+
 	}
 	else
 	{
