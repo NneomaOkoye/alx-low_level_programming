916d7f2c44c487cf71ec1e8b2828c582ad1dab38
NneomaOkoye <okoyenneoma1@gmail.com>
2022-12-16 15:43:55 -0800

    task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -3,10 +3,10 @@
 /**
  * print_remaining_days - takes a date and prints how many days are
  * left in a year, taking leap years into account
+ *
  * @month: month in number format
  * @day: day of month
  * @year: year
- * Return: void
  */
 
 void print_remaining_days(int month, int day, int year)
