888f01a91237018de8b0ef4a5d7a9b906a2b528e
root <root@DESKTOP-IJ3E97T>
2022-12-20 16:46:12 +0100

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * reset_to_98 - updates the value of n to 98
+ * reset_to_98 - update the value of n to 98
  * @n: integer pointer to a variable to be updated
- * *
- * Return: nothing
+ *
+ * Return: 0
  */
 
 void reset_to_98(int *n)
 {
-	*n =98;
+	*n = 98;
 }
