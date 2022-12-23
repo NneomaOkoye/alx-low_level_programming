514f48053d2c61297f8225d1e147242427fa610b
root <root@DESKTOP-IJ3E97T>
2022-12-23 04:34:44 +0100

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,30 +2,25 @@
 
 /**
  * _strcat - concecrate two strings
- * @dest:input value
- * @src: input value
- *
- * Return: void
+ * @dest:char
+ * @src: char
+ * Return:char
  */
-
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	char *s = dest;
 
-	i = 0;
-	while (dest[i] != '\0')
+	while (*dest != '\0')
 	{
-		i++
-	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
+		dest++;
 	}
 
-	dest[i] = '\0';
-	return (dest);
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (s);
 }
