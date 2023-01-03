3641ec97f824decb0213ccb8c334d2c958e1363e
NneomaOkoye <okoyenneoma1@gmail.com>
2023-01-03 05:45:17 +0100

    task

---
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to rpint
+ *
+ * Return: Nothing
+ */
+
