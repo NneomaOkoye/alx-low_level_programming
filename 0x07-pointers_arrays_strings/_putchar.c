8361eaa6744cf45f0c56b7d91700950981e43780
NneomaOkoye <okoyenneoma1@gmail.com>
2023-01-03 15:34:53 -0800

    again

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,24 +1,13 @@
-#include "main.h"
 #include <unistd.h>
+
 /**
  * _putchar - writes the character c to stdout
-<<<<<<< HEAD
  * @c: The character to print
- *
- * Return: On sucess 1.
+ * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
+
 {
-	return (write(1, &c, 1)):
-=======
- * @c: The character to [rint
- *
- * Return: On sucess 1.
- * on error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
->>>>>>> 03011cacaf349febd9df7fc0a79ef24395f1fda1
+	return (write(1, &c, 1))
 }
