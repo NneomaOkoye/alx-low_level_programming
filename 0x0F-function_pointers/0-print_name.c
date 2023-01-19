591dcaea442603bda0f9e02a93e0450906d1a563
NneomaOkoye <okoyenneoma1@gmail.com>
2023-01-19 01:35:33 -0800

    done

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,11 +5,10 @@
  * print_name - a function that prints a name
  * @name: pointer name to print
  * @f: function to print name
- * Return: nothing
  */
-void print_name(char *name, void (*f)(char *name))
+void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-
-		f(name);
+	if (!name || !f)
+		return;
+	f(name);
 }
