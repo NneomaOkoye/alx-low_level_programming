5925c24aeca015203b2262ea3a17842d90b28f17
NneomaOkoye <okoyenneoma1@gmail.com>
2023-01-19 01:39:42 -0800

    trying

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,9 +2,9 @@
 #include "function_pointers.h"
 
 /**
- * print_name - a function that prints a name
- * @name: pointer name to print
- * @f: function to print name
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to the printing function
  */
 void print_name(char *name, void (*f)(char *))
 {
