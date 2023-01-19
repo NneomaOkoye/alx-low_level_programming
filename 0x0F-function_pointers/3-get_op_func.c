8234dee828f8ea2a68c97115e8c3cf733f76d957
NneomaOkoye <okoyenneoma1@gmail.com>
2023-01-18 20:53:12 -0800

    done

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,13 +17,14 @@ int (*get_op_func(char *s))(int, int)
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
-		{ NULL, NULL}
+		{NULL, NULL}
 	};
-	int i = 0;
+	int i;
 
+	i = 0;
 	while (i < 5)
 	{
-		if (strcmp(s, ops[i].op, s) == 0)
+		if (strmp(s, ops[i].op, s) == 0)
 			return (ops[i].f);
 		i++;
 	}
