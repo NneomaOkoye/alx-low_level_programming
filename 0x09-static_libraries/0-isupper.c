d32e7853df5963be68b1026e31b02d9b5bb8083f
NneomaOkoye <okoyenneoma1@gmail.com>
2023-01-27 14:55:01 -0800

    task

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -2,15 +2,10 @@
 
 /**
  * _isupper - upppercase character
- * @c: input character
- *
- * Return: 1 or 0
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
-
 {
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-		return (0);
+	return (c >= 'A' && c <= 'Z');
 }
--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,14 +1,11 @@
 #include "main.h"
 
 /**
- * _isdigit - write a function that checks for a didgit (0 through 9)
- * @c: char to check
- * Return: 0 or 1
+ * _isdigit - checks for a didgit (0 through 9)
+ * @c: int to checked
+ * Return: 1 if c is a digit, 0 otherwise
  */
 int _isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
-		return (1);
-	else
-		return (0);
+	return (c >= '0' && c <= '9')
 }
